0599c1fef0a259935f5b946cec0ca63c22a3d80e
MagneticHamster <hector.morlet@gmail.com>
2014-04-12 23:52:29 +0800

    Made pixelColor.c work + updated stuff done in notes

--- pixelColor.c
+++ pixelColor.c
@@ -16,24 +16,30 @@
 #define MAX_STEPS 255
 #define MIN_STEPS 1
 
+#define RED_LERP_START 
+#define RED_LERP_END
+#define BLUE_LERP_START
+#define BLUE_LERP_END
+#define GREEN_LERP_START
+#define GREEN_LERP_END
 
-// TODO here the other function that is supposed to be in here that
-// Richard did in his demo
 
+static int lerp(start, end, time);
 
 unsigned char stepsToRed(int steps){
-   // Place holder return value
-   return steps % 30;
+   return lerp(RED_LERP_START, RED_LERP_END, steps/MAX_STEPS);
 }
 
 
 unsigned char stepsToBlue(int steps){
-   // Place holder return value
-   return steps % 30;
+   return lerp(BLUE_LERP_START, BLUE_LERP_END, steps/MAX_STEPS);
 }
 
 
 unsigned char stepsToGreen(int steps){
-   // Place holder return value
-   return steps % 30;
+   return lerp(GREEN_LERP_START, GREEN_LERP_END, steps/MAX_STEPS);
+}
+
+static int lerp(start, end, time) {
+   return start+(end-start)*time;
 }
